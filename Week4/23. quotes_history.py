11b7e95ce91ca7fe4789ebe78973c24559efd26f
HawkingLaugh <chanhugo@icloud.com>
2021-02-01 14:12:54 +0900

    move the elements into try block

--- Week4/23. quotes_history.py
+++ Week4/23. quotes_history.py
@@ -8,12 +8,12 @@ def retrieve_quotes_historical(stock_code):
     url = 'http://finance.yahoo.com/quote/%s/history?p=%s' % (stock_code, stock_code)
     try:
         r = requests.get(url)
+        m = re.findall('"HistoricalPriceStore":{"prices":(.*?),"isPending"', r.text)
+        if m:
+            quotes = json.loads(m[0])     # m = ['[{...},{...},...]']
+            quotes = quotes[::-1]
     except ConnectionError as err:
         print(err)  
-    m = re.findall('"HistoricalPriceStore":{"prices":(.*?),"isPending"', r.text)
-    if m:
-        quotes = json.loads(m[0])     # m = ['[{...},{...},...]']
-        quotes = quotes[::-1]
     return  [item for item in quotes if 'type' not in item]
  
 quotes = retrieve_quotes_historical('AXP')
